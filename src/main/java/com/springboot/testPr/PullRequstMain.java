a2c853953af0850dd933ef289a541391be3f8ec2
yeshuai <shuai.ye@china.talk.com>
2021-09-18 10:07:09 +0800

    test pr

---
+++ src/main/java/com/springboot/testPr/PullRequstMain.java
@@ -0,0 +1,13 @@
+package com.springboot.testPr;
+
+/**
+ * @Author yeshuai
+ * @Date 2021/9/18
+ **/
+public class PullRequstMain {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
